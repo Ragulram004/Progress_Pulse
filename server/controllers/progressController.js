025b123c0b6df360c1f0764d3b8120a89e36a4a9
Ragulram <ragulram0427@gmail.com>
2024-08-01 20:10:55 +0530

    Added backend

---
+++ server/controllers/progressController.js
@@ -0,0 +1,93 @@
+const Progress = require('../models/progressModel')
+const mongoose = require('mongoose')
+
+//get all progresses
+const getProgresses = async(req,res)=>{
+  const progresses = await Progress.find({}).sort({createdAt:-1})
+  res.status(200).json(progresses)
+}
+
+
+//get a single progress
+const getProgress = async (req,res)=>{
+  const {id} = req.params
+
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).json({error:'No Such progress'})
+  }
+  const progress = await Progress.findById(id)
+
+  if(!progress){
+    return res.status(404).json({error:"No sucn Progress"})
+  }
+
+  res.status(200).json(progress)
+}
+
+
+//create new progress
+const createProgress = async (req,res)=>{
+  const {title,hrs,discription} = req.body
+
+  let emptyFields = []
+  if(!title){
+    emptyFields.push('title')
+  }
+  if(!hrs){
+    emptyFields.push('hrs')
+  }
+  if(!discription){
+    emptyFields.push('discription')
+  }
+
+  if(emptyFields.length > 0){
+    return res.status(400).json({error:'Please fill in all the fields',emptyFields})
+  }
+  //add a document to DB
+  try{
+    const progress = await Progress.create({title,hrs,discription})
+    res.status(200).json(progress)
+  }catch(error){
+    res.status(400).json({error:error.message})
+  }
+}
+
+//delete a progress
+const deleteProgress = async(req,res)=>{
+  const {id} = req.params
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).json({error:'No Such progress'})
+  }
+
+  const progress = await Progress.findOneAndDelete({_id: id})
+  if(!progress){
+    return res.status(404).json({error:"No sucn Progress"})
+  }
+  res.status(200).json(progress)
+}
+
+
+//update progress
+const updateProgress = async(req,res)=>{
+  const {id} = req.params
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).json({error:'No Such progress'})
+  }
+  const progress = await Progress.findOneAndUpdate({_id:id},{
+    ...req.body
+  })
+  if(!progress){
+    return res.status(404).json({error:"No sucn Progress"})
+  }
+
+  res.status(200).json(progress)
+}
+
+
+module.exports = {
+  createProgress,
+  getProgresses,
+  getProgress,
+  deleteProgress,
+  updateProgress
+}
