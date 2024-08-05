644737f6fa6195dc2f2a989667b1c62c076690fe
Ragulram <ragulram0427@gmail.com>
2024-08-05 22:56:33 +0530

    login logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,7 +7,17 @@ const createToken = (_id) =>{
 
 //loginController 
 const userLogin = async(req,res) =>{
-  res.send('login msg')
+  const {email,password} = req.body
+
+  try{
+    const user = await User.login(email,password)
+    const token = createToken(user._id)
+
+    res.status(200).json({email,token})
+  }
+  catch(err){
+    res.status(400).json({error: err.message})
+  }
 }
 
 //signupController
--- server/models/userModel.js
+++ server/models/userModel.js
@@ -37,7 +37,7 @@ userSchema.statics.signup = async function(email,password) {
   if(exists){
     throw Error('Email already in use')
   }
-
+  // salt => not same encrypted for same passwords 
   const salt = await bcrypt.genSalt(10)
   const hash = await bcrypt.hash(password,salt)
   const user = await this.create({email,password:hash})
@@ -46,6 +46,24 @@ userSchema.statics.signup = async function(email,password) {
 }
 
 //static login method
+userSchema.statics.login = async function(email,password) {
+  
+  if(!email || !password){
+    throw Error ('Please fill in all the fields')
+  }
+
+  const user = await this.findOne({email})
+  if(!user){
+    throw Error ('User Not Found')
+  }
+
+  const passmatch = await bcrypt.compare(password,user.password)
+  if(!passmatch){
+    throw Error ('Incorrect Password')
+  }
+
+  return user
+}
 
 
 module.exports = mongoose.model('User',userSchema)
